07ddd7f2d2b978bcec7f025a9590520acd4f9ab6
Ubuntu <ubuntu@ip-172-31-9-249.us-east-2.compute.internal>
2023-03-26 19:33:50 +0000

    fix to recognize long audio

--- api/routers/line.py
+++ api/routers/line.py
@@ -15,7 +15,19 @@ from linebot.models import (
     MessageEvent, FileMessage, TextSendMessage, AudioMessage, VideoMessage, FlexSendMessage
 )
 import os
+import ffmpeg
+import srt
+from tempfile import NamedTemporaryFile
 from .errors import TranscriptionFailureError, FileSizeError, FileExtensionError, FileCorruptionError, UsageLimitError
+import logging
+
+# Logging related
+logger = logging.getLogger("line-logger")
+handler = logging.FileHandler("../../logs/line.log")
+handler.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
 
 LINEAPI_ACCESS_TOKEN = os.getenv("LINEAPI_ACCESS_TOKEN")
 LINEAPI_SECRET = os.getenv("LINEAPI_SECRET")
@@ -132,9 +144,29 @@ def handle_message_content(event, message_content):
         try:
             user_id = event.source.user_id
             text, additional_comment = transcribe(audio_file_path, matched_extension, user_id)
-            line_bot_api_client.reply_message(
-                event.reply_token,
-                TextSendMessage(text=text + "\n\n" + additional_comment if additional_comment else text))
+            text = text + "\n\n" + additional_comment if additional_comment else text
+            text_len = len(text)
+            if text_len < 5000:
+                line_bot_api_client.reply_message(
+                    event.reply_token,
+                    TextSendMessage(text=text))
+            else:
+                text_start_index = 0
+                processed_text_len = 0
+                count = 0
+                while processed_text_len < text_len:
+                    text_end_index = min(text_start_index + 5000, text_len)
+                    if count == 0:
+                        line_bot_api_client.reply_message(
+                            event.reply_token,
+                            TextSendMessage(text=text[text_start_index:text_end_index]))
+                    else:
+                        line_bot_api_client.push_message(
+                            user_id,
+                            TextSendMessage(text=text[text_start_index:text_end_index]))
+                    text_start_index = text_end_index
+                    processed_text_len += 5000
+                    count += 1
             if additional_comment is not None:
                 line_bot_api_client.push_message(
                     user_id,
@@ -169,43 +201,84 @@ def handle_message_content(event, message_content):
             TextSendMessage(text="対応していないファイル形式のようじゃの・・・\n以下のファイル形式で試すのじゃ!\n" + ", ".join(ACCEPT_FILE_EXTENSIONS)))
 
 
+def get_chunk_audio_file(audio_file_path, format, start_msec, duration_msec):
+    with NamedTemporaryFile("w+b", suffix=f".{format}") as f:
+        ffmpeg_cmd = (
+            ffmpeg
+            .input(audio_file_path, ss=start_msec / 1000, t=duration_msec / 1000)
+            .output(f.name, acodec='copy')
+            .overwrite_output()
+        )
+        ffmpeg_cmd.run(quiet=True)
+        audio_file = open(f.name, "rb")
+    return audio_file
+
+def get_audio_duration_msec(audio_file_path):
+    # NOTE: By using ffmpeg, hopefully we can reduce the memory usage
+    result = ffmpeg.probe(audio_file_path)
+    duration = float(result['format']['duration'])
+    return duration * 1000
+
 def transcribe(audio_file_path, extension, user_id):
     try:
-        audio = AudioSegment.from_file(audio_file_path, extension)
+        audio_duration_msec = get_audio_duration_msec(audio_file_path)
     except Exception as e:
         raise FileCorruptionError
+    result_text = ""
     additional_comment = None
-    duration_sec = audio.duration_seconds
+
     data = supabase_client.table('user_info').select('remaining_sec').filter('id', 'eq', user_id).execute().data
     if len(data) == 0:
         # create new user first
-        supabase_client.table('user_info').insert({'id': user_id}).execute()
+        supabase_client.table('user_info').insert({'id': user_id, 'remaining_sec': 300}).execute()
         data = supabase_client.table('user_info').select('remaining_sec').filter('id', 'eq', user_id).execute().data
+
     old_remaining_sec = data[0]['remaining_sec']
-    new_remaining_sec = old_remaining_sec - duration_sec
-    # check recognition is possible
+    new_remaining_sec = old_remaining_sec - audio_duration_msec / 1000
+
+    # check if recognition is possible
     if old_remaining_sec < 1:
         raise UsageLimitError(remaining_sec=0)
     else:
         # to avoid data competition, update remaining_sec ASAP
         supabase_client.table('user_info').upsert({'id': user_id, 'remaining_sec': max(new_remaining_sec, 0)}).execute()
-    # check audio cut is necessary
+    # check if audio cut is necessary
     if new_remaining_sec < 0:
-        audio = audio[:math.floor(old_remaining_sec * 1000)]
+        audio_duration_msec = old_remaining_sec * 1000
         additional_comment = "利用制限時間を超えたようじゃ、冒頭の" + get_remaining_time_text(old_remaining_sec) + "だけ書き起こしたぞ！"
-        # PyDub cannot export m4a file so convert it to mp3
-        audio.export(audio_file_path, format=extension if extension != "m4a" else "mp3")
-    audio_file= open(audio_file_path, "rb")
-    try:
-        # TODO: 以下の部分を非同期に行うことで他のユーザーのリクエストを処理できるようにする
-        transcript = openai.Audio.transcribe("whisper-1", audio_file, language="ja")
-        text = transcript.get("text", "")
-        return text, additional_comment
-    except Exception as e:
-        supabase_client.table('user_info').upsert({'id': user_id, 'remaining_sec': old_remaining_sec}).execute()
-        raise TranscriptionFailureError
-    finally:
-        audio_file.close()
+
+    CHUNK_DURATION_MSEC = 10 * 60 * 1000
+
+    start_msec = 0
+    processed_duration_msec = 0
+
+    while processed_duration_msec < audio_duration_msec:
+        duration_msec = min(CHUNK_DURATION_MSEC, audio_duration_msec - processed_duration_msec)
+        chunk_audio_file = get_chunk_audio_file(audio_file_path, extension, start_msec, duration_msec)
+
+        try:
+            # TODO: 以下の部分を非同期に行うことで他のユーザーのリクエストを処理できるようにする
+            chunk_result = openai.Audio.transcribe("whisper-1", chunk_audio_file, language="ja", response_format="srt")
+            srt_rows = srt.parse(chunk_result)
+            for i, row in enumerate(srt_rows):
+                text = row.content.strip()
+                if text == "":
+                    continue
+                if i == 0 and start_msec == 0:
+                    result_text += text
+                else:
+                    result_text += ("\n" + text)
+            processed_duration_msec += duration_msec
+            start_msec += duration_msec
+        except Exception as e:
+            supabase_client.table('user_info').upsert({'id': user_id, 'remaining_sec': old_remaining_sec}).execute()
+            raise TranscriptionFailureError
+        finally:
+            chunk_audio_file.close()
+
+    return result_text, additional_comment
+
+
 
 def get_remaining_time_text(remaining_sec):
     if remaining_sec < 60:
