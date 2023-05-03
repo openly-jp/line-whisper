afc6d7d472469ba53a3344d70784f09990e86eb8
Ubuntu <ubuntu@ip-172-31-0-172.us-east-2.compute.internal>
2023-05-03 14:57:58 +0000

    implement new health check

--- api/main.py
+++ api/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, Response, Depends
 from starlette.middleware.cors import CORSMiddleware
 from routers import line
+import tempfile
 
 
 
@@ -16,4 +17,10 @@ app.include_router(line.router)
 
 @app.get("/health")
 async def health():
+    try:
+        with tempfile.TemporaryFile() as fp:
+            fp = tempfile.TemporaryFile()
+            fp.write(b'health check')
+    except Exception as e:
+        return Response(status_code=503)
     return Response(status_code=200)
